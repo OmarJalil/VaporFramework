3eba2ff40d77903317b9010b15e809c236f14ac1
Jalil <omarjalilfierro@gmail.com>
2022-07-15 16:54:31 -0600

    [add] detail page

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -10,14 +10,32 @@ import Vapor
 struct WebsiteController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
         routes.get(use: indexHandler)
+        routes.get("acronyms", ":acronymId", use: acronymHandler)
     }
 
     func indexHandler(_ req: Request) async throws -> View {
-        let context = IndexContext(title: "Homepage")
+        let acronyms = try await Acronym.query(on: req.db).all()
+        let context = IndexContext(title: "Homepage", acronyms: acronyms.isEmpty ? nil : acronyms)
         return try await req.view.render("index", context)
+    }
+
+    func acronymHandler(_ req: Request) async throws -> View {
+        guard let acronym = try await Acronym.find(req.parameters.get("acronymId"), on: req.db) else {
+            throw Abort(.notFound)
+        }
+        let user = try await acronym.$user.get(on: req.db)
+        let context = AcronymContext(title: acronym.long, acronym: acronym, user: user)
+        return try await req.view.render("acronym", context)
     }
 }
 
 struct IndexContext: Encodable {
     let title: String
+    let acronyms: [Acronym]?
+}
+
+struct AcronymContext: Encodable {
+    let title: String
+    let acronym: Acronym
+    let user: User
 }
